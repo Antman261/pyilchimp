9d55c3ae6c98d0739c8fc33ab11a787e555ce19e
Anthony Manning-Franklin <anthony@welleco.com>
2017-02-14 16:54:40 +0800

    Basic skeleton of project.

---
+++ src/mailchimp_manager.py
@@ -0,0 +1,94 @@
+import requests
+import simplejson as json
+
+
+class MailchimpManager(object):
+    def __init__(self, api_key, server, api_version='3.0', auth_user='a'):
+        self.api_key = api_key
+        self.server = server
+        self.api_version = api_version
+        self.auth_user = auth_user
+        self.campaigns = CampaignManager(self)
+
+
+class ResourceManager(object):
+    def __init__(self, parent):
+        self.name = self.__class__.__name__
+        self.subject_name = self.name[:-7]
+        self.subject_class = globals()[self.subject_name]
+        self.path = self.subject_name.lower()+'s'
+        self.parent = parent
+
+    def _build_path(self, resource):
+        return 'https://{}.api.mailchimp.com/{}/{}'.format(self.parent.server,
+                                                           self.parent.api_version,
+                                                           resource)
+
+    def _request(self, resource, filters=None, type='GET', post_data={}):
+        url = self._build_path(resource)
+        if type == 'GET':
+            res = requests.get(url, auth=(self.parent.auth_user, self.parent.api_key),
+                               params=filters)
+            if res.status_code == 200:
+                return json.loads(res.text, use_decimal=True)
+            print res, url
+            return False
+
+    def get(self, id_or_resource, filters=None):
+        if isinstance(id_or_resource, self.subject_class):
+            resource = id_or_resource
+            data = self._request(resource, filters)
+            return resource._update_with_dict(data)
+        else:
+            id = id_or_resource
+            resource_path = '{}/{}'.format(self.path, id)
+            data = self._request(resource_path, filters)
+            return self.subject_class()._update_with_dict(data)
+
+    def all(self, filters=None):
+        data = self._request(self.path, filters)
+        key_name = self.subject_name.lower()+'s'
+        result_list = []
+        for item in data[key_name]:
+            result_list.append(self.subject_class(data_dict=item))
+        return result_list
+
+
+class CampaignManager(ResourceManager):
+    pass
+
+
+class Resource(object):
+    """
+    Resources in Mailchimp can be queried, filtered, AND perform actions.
+    """
+    def __init__(self, id=None, data_dict=None):
+        self.id = id
+        self.name = self.__class__.__name__
+        if data_dict is not None:
+            self._update_with_dict(data_dict)
+
+    def get_resource_path(self):
+        path = self.name.lower()+'s'
+        if self.id is not None:
+            return '{}/{}'.format(path, self.id)
+
+    def __str__(self):
+        return self.get_resource_path()
+
+    def __repr__(self):
+        return '<{}: {}>'.format(self.name, str(self))
+
+    def _update_with_dict(self, data):
+        for key, value in data.items():
+            setattr(self, key, value)
+
+
+class Campaign(Resource):
+    """
+    http://developer.mailchimp.com/documentation/mailchimp/reference/campaigns/#
+    """
+    def __str__(self, human=False):
+        if human:
+            return self.settings['subject_line'].encode('ascii', 'ignore')
+        return self.get_resource_path()
---
+++ tests/test_mc_manager.py
@@ -0,0 +1,51 @@
+import unittest
+from api.lib.mailchimp_manager import *
+from hypothesis import given
+import hypothesis.strategies as st
+import os
+"""
+This test suite needs http mocking to speed up and modularise tests.
+
+Property based testing would be nice too but hard to find functions that are well
+enough isolated from the API to actually send it properties and expect any
+sensible behaviour
+"""
+
+
+class TestCampaignAll(unittest.TestCase):
+    def setUp(self):
+        self.mc_manager = MailchimpManager(os.environ['MC_APIKEY'], 'us3')
+
+    def test_campaign_manager_all(self):
+        campaigns = self.mc_manager.campaigns.all()
+        self.assertIsInstance(campaigns, list)
+        for item in campaigns:
+            self.assertIsInstance(item, Campaign)
+
+    def test_build_path(self):
+        campaigns = self.mc_manager.campaigns.all()
+        self.assertEqual(self.mc_manager.campaigns._build_path('campaigns/123'),
+                         'https://us3.api.mailchimp.com/3.0/campaigns/123')
+        for campaign in campaigns:
+            path = self.mc_manager.campaigns._build_path(campaign)
+            self.assertIn('us3.', path)
+            self.assertIn(campaign.id, path)
+            self.assertIn('campaigns', path)
+
+    def test_str_repr(self):
+        campaigns = self.mc_manager.campaigns.all()
+        for campaign in campaigns:
+            str_human = campaign.__str__(human=True)
+            str_machine = str(campaign)
+            str_repr = repr(campaign)
+            self.assertNotIn(campaign.id, str_human)
+            self.assertEqual('campaigns/{}'.format(campaign.id), str_machine)
+            self.assertIn('<', str_repr)
+            self.assertIn('>', str_repr)
+            self.assertIn('Campaign:', str_repr)
+
+    def test_filter(self):
+        campaigns = self.mc_manager.campaigns.all()
+        campaigns_short = self.mc_manager.campaigns.all(filters={'count': 5})
+        self.assertEqual(len(campaigns), 10)
+        self.assertEqual(len(campaigns_short), 5)
